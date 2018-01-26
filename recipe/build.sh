bd3558eee486d8730510f8200cb567c3fb6ab8dd
Bill Ladwig <ladwig@ucar.edu>
2018-01-25 17:33:03 -0700

    1.1.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd ./fortran
+cd fortran/build_help
+gfortran -o sizes -fopenmp omp_sizes.f90
+python sub_sizes.py
+
+cd ..
 gfortran -E ompgen.F90 -fopenmp -cpp -o omp.f90
 f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature
 cd ..
@@ -12,3 +16,5 @@ fi
 $PYTHON setup.py config_fc --f90flags="-mtune=generic -fopenmp" build_ext --libraries="gomp" build
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+
+
