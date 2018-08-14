ca130ba274946c97bd60b59288c72fc0fe400a6b
Kevin Hallock <hallock@ucar.edu>
2018-08-14 14:36:49 -0600

    Removed unnecessary LDFLAGS in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,12 +13,6 @@ gfortran -E ompgen.F90 -fopenmp -cpp -o omp.f90
 f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature 
 cd ..
 
-if [ `uname` == Darwin ]; then
-    export LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
-fi
-
-export LDFLAGS="$LDFLAGS -nostartfiles"
-
 $PYTHON setup.py config_fc --f90flags="-mtune=generic -fopenmp" build_ext --libraries="gomp" build
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
