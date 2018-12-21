afd598167a5f49765505eaeedca323e90d0335a5
Bill Ladwig <ladwig@ucar.edu>
2018-12-21 12:10:17 -0700

    1.3.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ python sub_sizes.py
 
 cd ..
 $FC -E ompgen.F90 -fopenmp -cpp -o omp.f90
-f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature 
+#f2py *.f90 -m _wrffortran -h wrffortran.pyf --overwrite-signature 
 cd ..
 
 $PYTHON setup.py config_fc --f90flags="-mtune=generic -fopenmp" build_ext --libraries="gomp" build
